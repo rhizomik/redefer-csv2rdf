2b9f108cc565d32db1b4b16f1c13f36905ba8343
Raul Roca <alcarras.raulroca@gmail.com>
2020-05-31 10:12:06 +0200

    changes

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-//Install express server
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/rdf-transformer'));
+app.use(express.static('./dist/rdf-transformer'));
 
-app.get('/*', function(req,res) {
-    console.log(" Fetching from.." + __dirname);
-res.sendFile(path.join(__dirname+'/dist/rdf-transformer/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/rdf-transformer/'}),
+);
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
